7c0fc3b94502a5cb9a33c47d6ebb2ceef415dd66
Sharon <sharonmytube@gmail.com>
2019-08-29 15:36:21 +0300

    types

---
+++ Types.js
@@ -0,0 +1,15 @@
+//String
+var myName = 'Max';
+//Number
+var myAge = 27;
+//boolean
+var hasHobbies = false;
+//assing types
+var myRealage;
+myRealage = 27;
+myRealage = '27';
+var myRealage2;
+myRealage2 = 37;
+//Array
+var hobbies = ["coocking", "Sport"];
+console.log(hobbies[0]);
