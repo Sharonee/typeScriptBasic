dcd98abb8f12618237427283697f4e40612ca5aa
Sharon <sharonmytube@gmail.com>
2019-08-29 16:27:16 +0300

    array of any

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ myRealage = 27;
 myRealage = '27';
 var myRealage2;
 myRealage2 = 37;
-//Array
-var hobbies = ["coocking", "Sport"];
+//Array of string
+var hobbies = ["cooking", "Sport"];
+console.log(typeof hobbies);
+console.log(hobbies[0]);
+//Array of any
+var hobbies2 = ["cooking2", "Sport2"];
+hobbies2 = [100];
+console.log(typeof hobbies);
 console.log(hobbies[0]);
---
+++ app.ts
@@ -0,0 +1,30 @@
+//String
+let myName = 'Max';
+ 
+//Number
+let myAge = 27;
+
+//boolean
+let hasHobbies = false;
+
+//assing types
+let myRealage
+myRealage = 27;
+myRealage = '27';
+
+let myRealage2: number;
+myRealage2 = 37;
+
+//Array of string
+let hobbies = ["cooking", "Sport"];
+console.log(typeof hobbies);
+console.log(hobbies[0]);
+
+//Array of any
+let hobbies2: any[] = ["cooking2", "Sport2"];
+hobbies2 = [100];
+console.log(typeof hobbies);
+console.log(hobbies[0]);
+
+
+
